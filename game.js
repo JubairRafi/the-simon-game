2b11143fa8764c898650e45421f90c44d588f183
jobairRafi96 <jubairrafi96960@gmail.com>
2020-07-04 07:03:09 -0700

    animate flash effect and pattern added to array

--- game.js
+++ game.js
@@ -1,6 +1,19 @@
+//assign
 var buttonColours = ["red","blue","green","yellow"];
 var gamePattern=[];
+var userClickedPattern = [];
 
+//event
+$(".btn").click(function(){
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  //console.log(userClickedPattern);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
+
+
+//functions
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4); //randomNumber 0-3
 
@@ -10,10 +23,19 @@ function nextSequence(){
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  // audio from the array
+  playSound(randomChosenColour);
+}
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+function playSound(name){
+  // audio from the array
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-// $("#randomChosenColour").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+function animatePress(currentColor){
+  $("."+currentColor).addClass("pressed");
+
+        setTimeout(function() {
+        $("."+currentColor).removeClass("pressed");
+      }, 100);
+}
